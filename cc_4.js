3e558b4759e156bb932eb872c7e3a63a2c0ed8a7
Amanda Robles <amandarobles@usf.edu>
2025-02-01 13:37:27 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,11 @@ if (purchaseAmount > 100) {
     purchaseAmount -= discount;
 }
 console.log('Final amount after discount: $${purchaseAmount}');
+
+// task 2: For Loop
+let sales = [120, 85, 200, 150, 90];
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales [i];
+}
+console.log('Total Sales: $${totalSales}');
