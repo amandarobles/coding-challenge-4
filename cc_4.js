c9dad289fda0eba0422f11f2f4ccf5c10ebcceb0
Amanda Robles <amandarobles@usf.edu>
2025-02-01 13:50:06 -0500

    Task 6 - Product Listings

--- cc_4.js
+++ cc_4.js
@@ -39,3 +39,9 @@ let employee = {
 for (let key in employee) {
     console.log('${key}: ${employee[key]}');
 }
+
+//task 6: For...Of Loop
+let products = ["Laptop", "Mouse", "Keyboard"];
+for (let product of products){
+    console.log('Product: ${product}');
+}
