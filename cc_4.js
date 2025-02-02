e7b5fb571d1b76998922f6a7a82ae38b34c0568a
Amanda Robles <amandarobles@usf.edu>
2025-02-02 11:18:33 -0500

    Updated cc_4js

--- cc_4.js
+++ cc_4.js
@@ -1,10 +1,11 @@
 // task 1: Customer Discounts
-let purchaseAmount = 120;
+let purchaseAmount = 200;
+let finalAmount = purchaseAmount;
 if (purchaseAmount > 100) {
-    let discount = purchaseAmount * 0.1;
-    purchaseAmount -= discount;
+    finalAmount = purchaseAmount * 0.9;
+    console.log("Discount applied")
 }
-console.log('Final amount after discount: $${purchaseAmount}');
+console.log(`Final amount after discount: $${finalAmount}`);
 
 // task 2: For Loop
 let sales = [120, 85, 200, 150, 90];
@@ -12,12 +13,12 @@ let totalSales = 0;
 for (let i = 0; i < sales.length; i++) {
     totalSales += sales [i];
 }
-console.log('Total Sales: $${totalSales}');
+console.log(`Total Sales: $${totalSales}`);
 
 //task 3: While Loop
 let stock = 10;
 while (stock > 0) {
-    console.log('Stock remaining: ${stock}');
+    console.log(`Stock remaining: ${stock}`);
     stock--;
 }
 console.log("Stock depleted.");
@@ -25,7 +26,7 @@ console.log("Stock depleted.");
 //task 4: Do...While Loop
 let responses = 0;
 do {
-    console.log('Collected response #${responses + 1}');
+    console.log(`Collected response: ${responses}`);
     responses++;
 } while (responses < 3);
 
@@ -37,34 +38,39 @@ let employee = {
 };
 
 for (let key in employee) {
-    console.log('${key}: ${employee[key]}');
+    console.log(`${key}: ${employee[key]}`);
 }
 
 //task 6: For...Of Loop
 let products = ["Laptop", "Mouse", "Keyboard"];
-for (let product of products){
-    console.log('Product: ${product}');
+for (let product of products) {
+    console.log(`Product: ${product}`);
 }
 
 //task 7: forEach() Method
 let orders = [101, 102, 103];
 orders.forEach(order => {
-    console.log('Processing Order ID: ${order}');
+    console.log(`Processing Order ID: ${order}`)
 });
 
 //task 8: Function Declaration
 function calculateTax(amount, taxRate) {
     return amount * taxRate;
 }
-console.log('Tax on $200 at 10%: $${calculateTax(200, 0.1)}');
+let tax = calculateTax(1000, 0.07);
+console.log(`Tax: $${tax}`);
 
 //task 9: Function Expression
 const applyDiscount = function(price, discountPercentage) {
-    return price - (price * discountPercentage / 100);
+    return price - (price * (discountPercentage / 100));
 };
-console.log('Discounted Price: $${applyDiscount(100, 15)}');
+let discountedPrice = applyDiscount (100, 10)
+console.log(`Discounted Price: $${discountedPrice}`);
 
 //task 10: Arrow Function
-const calculatePoints = purchaseAmount => Math.floor(purchaseAmount / 10);
+const calculatePoints = (purchaseAmount) => {
+    return (purchaseAmount / 10);
+};
+let points = calculatePoints(200);
 
-console.log('Points Earned: ${calculatePoints(250)}');
+console.log(`Points Earned: ${points}`);
